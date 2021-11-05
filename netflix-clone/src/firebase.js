a812eaefeb9afd86882ded0831b2a02e15612349
Aditya Sinha <sinhadityaforever@gmail.com>
2021-11-06 01:27:06 +0530

    Initial commit

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAGiUETRv1-ZFDr20AvOSfawffLi1BX71E",
+  authDomain: "netflix-clone-141f3.firebaseapp.com",
+  projectId: "netflix-clone-141f3",
+  storageBucket: "netflix-clone-141f3.appspot.com",
+  messagingSenderId: "686940294855",
+  appId: "1:686940294855:web:0f9cba5a61bde8d8c3b8e2",
+};
